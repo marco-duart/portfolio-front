aab63ea279fcdd03b29b3dbcb87003fba87c6aef
Marco Aurélio <marcoaurelio_db@hotmail.com>
2024-07-01 12:16:25 -0300

    feat: linked new fonts

--- src/assets/styles/styled.d.ts
+++ src/assets/styles/styled.d.ts
@@ -4,8 +4,15 @@ declare module "styled-components" {
   export interface DefaultTheme {
     name: string;
     colors: {
+      primary: string;
+      secundary: string;
+      background: string;
     };
     fonts: {
+      playWrite: string;
+      openSans: string;
+      candal: string;
+      poppins: string;
     };
     fontSizes: {
     };
--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -3,8 +3,15 @@ import { DefaultTheme } from "styled-components";
 export const light: DefaultTheme = {
   name: "Default",
   colors: {
+    primary: "#161616",
+    secundary: "#8AFF00",
+    background: "#161616",
   },
   fonts: {
+    playWrite: "'Playwrite IT Moderna', cursive",
+    openSans: "'Open Sans', sans-serif",
+    candal: "'Candal', sans-serif",
+    poppins: "'Poppins', sans-serif;"
   },
   fontSizes: {
   },
