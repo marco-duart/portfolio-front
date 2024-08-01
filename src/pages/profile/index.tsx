128450a848b58f59f2521a942a8ce5d1de52e410
marco-duart <marcoaurelio_db@hotmail.com>
2024-07-31 21:52:53 -0300

    feat: new item button

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,45 +1,63 @@
-import * as S from "./styles"
-import * as CONSTANTS from "../../utils/constants/constants"
-import { ResumeForm } from "../../components/forms/resume-form"
-import { useEducationCrud, useExperienceCrud, useResumeCrud, useSkillCrud } from "../../hooks"
-import BaseCard from "../../components/shared/cards/base-card"
-import { ExperienceForm } from "../../components/forms/experience-form"
-import { EducationForm } from "../../components/forms/education-form"
-import { SkillForm } from "../../components/forms/skill-form"
-
+import React, { useState } from "react";
+import * as S from "./styles";
+import * as CONSTANTS from "../../utils/constants/constants";
+import { ResumeForm } from "../../components/forms/resume-form";
+import { useEducationCrud, useExperienceCrud, useResumeCrud, useSkillCrud } from "../../hooks";
+import BaseCard from "../../components/shared/cards/base-card";
+import { ExperienceForm } from "../../components/forms/experience-form";
+import { EducationForm } from "../../components/forms/education-form";
+import { SkillForm } from "../../components/forms/skill-form";
 
 export const Profile: React.FC = () => {
-  const { resume, createResume, updateResume } = useResumeCrud()
-  const { experiences, createExperience, updateExperience } = useExperienceCrud()
-  const { educations, createEducation, updateEducation } = useEducationCrud()
-  const { skills, createSkill, updateSkill } = useSkillCrud()
+  const { resume, createResume, updateResume } = useResumeCrud();
+  const { experiences, createExperience, updateExperience } = useExperienceCrud();
+  const { educations, createEducation, updateEducation } = useEducationCrud();
+  const { skills, createSkill, updateSkill } = useSkillCrud();
 
-  console.log("Experience: ", experiences, "/n Educations: ", educations, "/n Skills: ", skills)
+  const [showNewSkillForm, setShowNewSkillForm] = useState(false);
+
+  const handleAddNewSkill = () => {
+    setShowNewSkillForm(true);
+  };
+
+  const handleCancelNewSkill = () => {
+    setShowNewSkillForm(false);
+  };
+
   return (
     <S.Container>
       <S.Title>Perfil</S.Title>
       <ResumeForm resume={resume} onCancel={() => {}} onCreate={createResume} onEdit={updateResume} />
+      
       <div>
-      { experiences?.map((experience, index) => (
-        <BaseCard key={index}>
-          <ExperienceForm resumeId={resume?.id} experience={experience} onCreate={createExperience} onEdit={updateExperience} onCancel={() => {}} />
-        </BaseCard>
-      ))}
+        {experiences?.map((experience, index) => (
+          <BaseCard key={index}>
+            <ExperienceForm resumeId={resume?.id} experience={experience} onCreate={createExperience} onEdit={updateExperience} onCancel={() => {}} />
+          </BaseCard>
+        ))}
       </div>
+      
       <div>
-      { educations?.map((education, index) => (
-        <BaseCard key={index}>
-          <EducationForm resumeId={resume?.id} education={education} onCreate={createEducation} onEdit={updateEducation} onCancel={() => {}} />
-        </BaseCard>
-      ))}
+        {educations?.map((education, index) => (
+          <BaseCard key={index}>
+            <EducationForm resumeId={resume?.id} education={education} onCreate={createEducation} onEdit={updateEducation} onCancel={() => {}} />
+          </BaseCard>
+        ))}
       </div>
+      
       <div>
-      { skills?.map((skill, index) => (
-        <BaseCard key={index}>
-          <SkillForm resumeId={resume?.id} skill={skill} onCreate={createSkill} onEdit={updateSkill} onCancel={() => {}} />
-        </BaseCard>
-      ))}
+        {skills?.map((skill, index) => (
+          <BaseCard key={index}>
+            <SkillForm resumeId={resume?.id} skill={skill} onCreate={createSkill} onEdit={updateSkill} onCancel={() => {}} />
+          </BaseCard>
+        ))}
+        {showNewSkillForm && (
+          <BaseCard>
+            <SkillForm resumeId={resume?.id} onCreate={createSkill} onEdit={updateSkill} onCancel={handleCancelNewSkill} />
+          </BaseCard>
+        )}
+        <S.AddButton onClick={handleAddNewSkill}>Adicionar Nova Habilidade</S.AddButton>
       </div>
     </S.Container>
-  )
-}
+  );
+};
--- src/pages/profile/styles.ts
+++ src/pages/profile/styles.ts
@@ -1,8 +1,8 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Container = styled.div`
   min-height: 40%;
-  background-color: ${ props => props.theme.colors.white };
+  background-color: ${(props) => props.theme.colors.white};
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -10,9 +10,20 @@ export const Container = styled.div`
   div {
     display: flex;
   }
-`
+`;
 
 export const Title = styled.h1`
   width: 100%;
   text-align: center;
-`
+`;
+
+export const AddButton = styled.button`
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  background-color: ${(props) => props.theme.colors.primary};
+  color: ${(props) => props.theme.colors.white};
+`;
